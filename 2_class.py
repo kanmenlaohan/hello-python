6649fe5bf5983b5891e3ed6cd405245ef02e7b97
Lincoln Zhang <zhangal@amazon.com>
2018-07-12 14:39:30 -0700

    commit

---
+++ 2_class.py
@@ -0,0 +1,19 @@
+#!usr/bin/env python3
+class calculator(x,y):
+
+    def addition(self):
+        added = self.x + self.y
+        print(added)
+    def substraction(self):
+        sub = self.x - self.y
+        print(sub)
+    def multiplication(self):
+        mult = self.x * self.y
+        print(mult)
+    def division(self):
+        div = self.x/self.y
+        print(div)
+
+calculator.multiplication(3,5)
+calculator.division(44,9)
+
